2831670d09c63eb492ca8bdb8446cf1cc27ca55c
bqasim381 <bqasim381@gmail.com>
2017-01-09 01:37:18 +0000

    hello world test

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 require('babel-polyfill');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const { Provider } = require('redux');
 
+const App = require('./components/app')
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<App>Hello World</App>, document.getElementById('app'));
+});
